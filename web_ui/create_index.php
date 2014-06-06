120200011e273dece5617970ab187c61f43cb4f7
fiedukow <fiedukow@gmail.com>
2014-06-06 20:12:20 +0200

    Fix inside indexing - synonyms of words are now using correct literal_id.

--- web_ui/create_index.php
+++ web_ui/create_index.php
@@ -5,7 +5,7 @@ mysql_select_db('WEDT');
 mysql_query("SET NAMES utf8");
 
 echo "<li>Rewriting words as q=1.0 synonyms...</li>";
-$result = mysql_query("SELECT `id` from `words`");
+$result = mysql_query("SELECT `id`, `literal_id` from `words`");
 
 $insert_q = "INSERT INTO  `WEDT`.`synonyms` ( `word_id` ,`literal_id` , `quality` , `display_count` ) ";
 $insert_q .= "VALUES";
@@ -13,7 +13,7 @@ $first = true;
 while($row = mysql_fetch_array($result)) {
   if (!$first)
     $insert_q .= ",";
-  $insert_q .= " ('".$row[0]."', '".$row[0]."', '1', '0')";
+  $insert_q .= " ('".$row[0]."', '".$row[1]."', '1', '0')";
   $first = false;
 }
 if (!$first) {
